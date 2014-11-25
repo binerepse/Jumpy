24eaa1b8b4786d107ec558f3ce76155209f53248
binerepse <bine.repse@gmail.com>
2014-11-25 23:18:57 +0100

    Homescreen, malo three.js
    Prikaže se kocka, z osvetlitvijo

---
+++ scripts/jumpy.js
@@ -0,0 +1,49 @@
+var scene, camera, renderer, cube;
+	   $(document.getElementById("StartButton")).click(function () {
+        $("#homescreen").hide();
+        $("#canvas").show();
+        init();
+		animate();
+    });
+	
+
+	function init(){
+		//SCENE//
+		scene = new THREE.Scene();
+		var WIDTH = window.innerWidth;
+		var HEIGHT = window.innerHeight;
+		//RENDERER//
+		var canvas = document.getElementById("canvas");
+		renderer = new THREE.WebGLRenderer({canvas : canvas});
+		renderer.setSize(WIDTH,HEIGHT);
+		document.body.appendChild(renderer.domElement);
+		//CAMERA//
+		camera = new THREE.PerspectiveCamera(45, WIDTH/HEIGHT, 0.1, 20000);
+		camera.position.set(0,0,+7);
+		scene.add(camera);
+		//RESIZE HANDLING//
+		window.addEventListener('resize', function(){
+			var WIDTH = window.innerWidth;
+			var HEIGHT = window.innerHeight;
+			renderer.setSize(WIDTH, HEIGHT);
+			camera.aspect = WIDTH/HEIGHT;
+			camera.updateProjectionMatrix(); 
+		});
+		//LIGHTING//
+		renderer.setClearColorHex(0x333F47, 1);
+		var light = new THREE.SpotLight(0xffffff);
+		light.position.set(0,0,100);
+		scene.add(light);	
+		//LOAD GEOMETRY//
+		var geometry = new THREE.BoxGeometry(1,1,1);
+		var material = new THREE.MeshPhongMaterial({color: 0x0eff80});
+		cube = new THREE.Mesh(geometry, material);
+		scene.add(cube);
+
+	}
+
+	function animate(){
+		requestAnimationFrame(animate);
+		renderer.render(scene, camera);
+		controls.update();
+	}
